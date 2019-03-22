3d3c23d929bf04a586f64dee2865d371977a6972
NelsonEAX <nelsoneax@yandex.ru>
2019-03-22 22:44:34 +0500

    vue add vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
+import ru from 'vuetify/es5/locale/ru'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#ee44aa',
+    secondary: '#424242',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  },
+  options: {
+    customProperties: true
+  },
+  iconfont: 'fa4',
+  lang: {
+    locales: { ru },
+    current: 'ru'
+  },
+})
