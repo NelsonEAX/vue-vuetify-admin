06865fdaf0f0cce3a60c4fa1cd714c5a411ec82b
NelsonEAX <nelsoneax@yandex.ru>
2019-03-23 00:40:39 +0500

    add vuetify template https://vuetifyjs.com/en/examples/layouts/googleContacts

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
+/* eslint-disable */
+//import 'normalize.css/normalize.css';
+import 'vuetify/dist/vuetify.min.css'
+/* icons */
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import 'font-awesome/css/font-awesome.css';
+/* fonts */
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+
 import Vue from 'vue';
-import './plugins/vuetify';
+import Vuetify from 'vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 /* import VueI18n from 'vue-i18n'; */
 import './registerServiceWorker';
-import 'roboto-fontface/css/roboto/roboto-fontface.css';
-import 'font-awesome/css/font-awesome.css';
+
+
+Vue.use(Vuetify, {
+  // iconfont: 'mdi' || 'md' || 'mdi' || 'fa' || 'fa4'
+});
 
 Vue.config.productionTip = false;
 
