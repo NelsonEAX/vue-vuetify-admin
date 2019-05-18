d9268fe594c55dcc7d83089a4e9528864430cbc7
NelsonEAX <nelsoneax@yandex.ru>
2019-05-18 13:48:50 +0500

    Travis: webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,25 @@
+'use strict';
+const path = require('path');
+const defaultSettings = require('./src/settings.js');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+};
+
+const name = defaultSettings.title || 'Vue Vuetify Admin'; // page title
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vue-vuetify-admin/'
-    : '/'
+    : '/',
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: name,
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  }
 };
